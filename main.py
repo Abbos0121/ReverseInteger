c85399cfb3b701e45ac5e852a935888c1c9faa2f
Abbos Madmurotov <abbossmadmurotov010120011@gmail.com>
2023-09-09 09:08:01 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        stringX = str(abs(x))
+        revString = stringX[::-1]
+        output = int(revString)
+        if(output > 2147483647):
+            return 0
+        return output if x > 0 else (-1)*output
